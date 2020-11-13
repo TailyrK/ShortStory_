3c9b30ad895e38b17df07bd6b953c37bf5a329fd
a.m.student <tailyrdebose2017@gmail.com>
2020-11-13 10:49:22 -0600

    after activityy

---
+++ app/src/main/java/com/raywenderlich/shortstory_/endStory.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.shortstory_
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class endStory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end_story)
+    }
+}
